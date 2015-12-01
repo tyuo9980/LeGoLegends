6d61e489f830d00f760c66a7a0a14342ff4c0c2b
Peter Li <tyuo9980@gmail.com>
2015-11-30 21:01:45 -0500

    added game endpoint

--- api_base.go
+++ api_base.go
@@ -1,120 +1,122 @@
 package legolegends
 
 import (
-    "encoding/json"
-    "fmt"
-    "log"
-    "net/http"
-    "strconv"
-    "strings"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
 )
 
 var (
-    Region string
-    ApiKey string
-    Debug  bool = false
+	Region string
+	ApiKey string
+	Debug  bool = false
 )
 
 const (
-    CDN_ROOT string = "https://ddragon.leagueoflegends.com/cdn"
+	CDN_ROOT string = "https://ddragon.leagueoflegends.com/cdn"
 
-    MATCH_LIST string = "v2.2/matchlist/by-summoner/"
+	MATCH_LIST string = "v2.2/matchlist/by-summoner/"
 
-    MATCH string = "v2.2/match/"
+	MATCH string = "v2.2/match/"
 
-    SUMMONER_BY_NAME   string = "v1.4/summoner/by-name/"
-    SUMMONER_BY_ID     string = "v1.4/summoner/"
-    SUMMONER_NAME      string = "v1.4/summoner/"
-    SUMMONER_RUNES     string = "v1.4/summoner/"
-    SUMMONER_MASTERIES string = "v1.4/summoner/"
+	GAME string = "v1.3/game/by-summoner/"
 
-    STATIC_VERSION  string = "v1.2/versions/"
-    STATIC_CHAMPION string = "v1.2/champion/"
-    STATIC_ITEM     string = "v1.2/item/"
+	SUMMONER_BY_NAME   string = "v1.4/summoner/by-name/"
+	SUMMONER_BY_ID     string = "v1.4/summoner/"
+	SUMMONER_NAME      string = "v1.4/summoner/"
+	SUMMONER_RUNES     string = "v1.4/summoner/"
+	SUMMONER_MASTERIES string = "v1.4/summoner/"
+
+	STATIC_VERSION  string = "v1.2/versions/"
+	STATIC_CHAMPION string = "v1.2/champion/"
+	STATIC_ITEM     string = "v1.2/item/"
 )
 
 func NormalizeName(names ...string) []string {
-    var nameList []string
+	var nameList []string
 
-    for i, name := range names {
-        name = strings.Replace(name, " ", "", -1)
-        name = strings.ToLower(name)
-        nameList[i] = name
-    }
+	for i, name := range names {
+		name = strings.Replace(name, " ", "", -1)
+		name = strings.ToLower(name)
+		nameList[i] = name
+	}
 
-    return nameList
+	return nameList
 }
 
 func createApiUrl(endpoint string) string {
-    return fmt.Sprintf("https://%v.api.pvp.net/api/lol/%v/%v", Region, Region, endpoint)
+	return fmt.Sprintf("https://%v.api.pvp.net/api/lol/%v/%v", Region, Region, endpoint)
 }
 
 func createStaticUrl(endpoint string) string {
-    return fmt.Sprintf("https://%v.api.pvp.net/api/lol/static-data/%v/%v", Region, Region, endpoint)
+	return fmt.Sprintf("https://%v.api.pvp.net/api/lol/static-data/%v/%v", Region, Region, endpoint)
 }
 
 func createObserverUrl(endpoint string) string {
-    return fmt.Sprintf("https://%v.api.pvp.net/observer-mode/rest/%v", Region, endpoint)
+	return fmt.Sprintf("https://%v.api.pvp.net/observer-mode/rest/%v", Region, endpoint)
 }
 
 func createShardUrl(endpoint string) string {
-    return fmt.Sprintf("https://%v.api.pvp.net/shards/%v", Region, endpoint)
+	return fmt.Sprintf("https://%v.api.pvp.net/shards/%v", Region, endpoint)
 }
 
 func createArgs(keys string, argVals ...interface{}) string {
-    keyArray := strings.Split(keys, ",")
-    args := "api_key=" + ApiKey
+	keyArray := strings.Split(keys, ",")
+	args := "api_key=" + ApiKey
 
-    for i, arg := range argVals {
-        var val string = ""
-        switch arg.(type) {
-        case bool:
-            val = strconv.FormatBool(arg.(bool))
-        case int:
-            if arg.(int) != -1 {
-                val = strconv.Itoa(arg.(int))
-            }
-        case int64:
-            if arg.(int64) != -1 {
-                val = strconv.FormatInt(arg.(int64), 10)
-            }
-        default:
-            val = arg.(string)
-        }
+	for i, arg := range argVals {
+		var val string = ""
+		switch arg.(type) {
+		case bool:
+			val = strconv.FormatBool(arg.(bool))
+		case int:
+			if arg.(int) != -1 {
+				val = strconv.Itoa(arg.(int))
+			}
+		case int64:
+			if arg.(int64) != -1 {
+				val = strconv.FormatInt(arg.(int64), 10)
+			}
+		default:
+			val = arg.(string)
+		}
 
-        args += fmt.Sprintf("&%v=%v", keyArray[i], val)
-    }
+		args += fmt.Sprintf("&%v=%v", keyArray[i], val)
+	}
 
-    return args
+	return args
 }
 
 func decodeRequest(url string, v interface{}) error {
-    if Debug {
-        log.Println("decodeRequest: " + url)
-    }
+	if Debug {
+		log.Println("decodeRequest: " + url)
+	}
 
-    resp, err := http.Get(url)
-    if err != nil {
-        return err
-    }
-    defer resp.Body.Close()
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
 
-    err = json.NewDecoder(resp.Body).Decode(&v)
-    if err != nil {
-        return err
-    }
+	err = json.NewDecoder(resp.Body).Decode(&v)
+	if err != nil {
+		return err
+	}
 
-    return err
+	return err
 }
 
 func SetRegion(region string) {
-    Region = region
+	Region = region
 }
 
 func SetApiKey(key string) {
-    ApiKey = key
+	ApiKey = key
 }
 
 func SetDebug(debug bool) {
-    Debug = debug
+	Debug = debug
 }

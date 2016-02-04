73b6081734973211377e6e4f234bcf1358b1eabd
Peter Li <tyuo9980@gmail.com>
2016-02-04 15:22:29 -0500

    champion mastery endpoint

--- api_base.go
+++ api_base.go
@@ -1,197 +1,203 @@
 package legolegends
 
 import (
-    "encoding/json"
-    "errors"
-    "fmt"
-    "log"
-    "net/http"
-    "strconv"
-    "strings"
-    "time"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
 )
 
 var (
-    PlatformId string
-    ApiKey     string
-    Debug      bool = false
+	PlatformId string
+	ApiKey     string
+	Debug      bool = false
 
-    pidMap = map[string]string{
-        "na":   "NA1",
-        "euw":  "EUW1",
-        "eune": "EUN1",
-        "kr":   "KR",
-        "br":   "BR1",
-        "lan":  "LA1",
-        "las":  "LA2",
-        "oce":  "OC1",
-        "ru":   "RU",
-        "tr":   "TR1",
-    }
+	pidMap = map[string]string{
+		"na":   "NA1",
+		"euw":  "EUW1",
+		"eune": "EUN1",
+		"kr":   "KR",
+		"br":   "BR1",
+		"lan":  "LA1",
+		"las":  "LA2",
+		"oce":  "OC1",
+		"ru":   "RU",
+		"tr":   "TR1",
+	}
 
-    requestChannelMap = make(map[string]chan bool)
+	requestChannelMap = make(map[string]chan bool)
 )
 
 const (
-    CDN_ROOT string = "https://ddragon.leagueoflegends.com/cdn"
+	CDN_ROOT string = "https://ddragon.leagueoflegends.com/cdn"
 
-    MATCH_LIST string = "v2.2/matchlist/by-summoner/"
+	MATCH_LIST string = "v2.2/matchlist/by-summoner/"
 
-    MATCH string = "v2.2/match/"
+	MATCH string = "v2.2/match/"
 
-    GAME string = "v1.3/game/by-summoner/"
+	GAME string = "v1.3/game/by-summoner/"
 
-    CURRENT_GAME string = "observer-mode/rest/consumer/getSpectatorGameInfo/"
+	CURRENT_GAME string = "observer-mode/rest/consumer/getSpectatorGameInfo/"
 
-    LEAGUE_BY_SUMMONER string = "v2.5/league/by-summoner/"
-    LEAGUE_BY_TEAM     string = "v2.5/league/by-team/"
-    LEAGUE_CHALLENGER  string = "v2.5/league/challenger/"
-    LEAGUE_MASTER      string = "v2.5/league/master"
+	CHAMPION_MASTERY string = "player/"
 
-    SUMMONER_BY_NAME   string = "v1.4/summoner/by-name/"
-    SUMMONER_BY_ID     string = "v1.4/summoner/"
-    SUMMONER_NAME      string = "v1.4/summoner/"
-    SUMMONER_RUNES     string = "v1.4/summoner/"
-    SUMMONER_MASTERIES string = "v1.4/summoner/"
+	LEAGUE_BY_SUMMONER string = "v2.5/league/by-summoner/"
+	LEAGUE_BY_TEAM     string = "v2.5/league/by-team/"
+	LEAGUE_CHALLENGER  string = "v2.5/league/challenger/"
+	LEAGUE_MASTER      string = "v2.5/league/master"
 
-    STATS_RANKED  string = "v1.3/stats/by-summoner/"
-    STATS_SUMMARY string = "v1.3/stats/by-summoner/"
+	SUMMONER_BY_NAME   string = "v1.4/summoner/by-name/"
+	SUMMONER_BY_ID     string = "v1.4/summoner/"
+	SUMMONER_NAME      string = "v1.4/summoner/"
+	SUMMONER_RUNES     string = "v1.4/summoner/"
+	SUMMONER_MASTERIES string = "v1.4/summoner/"
 
-    STATIC_VERSION  string = "v1.2/versions/"
-    STATIC_CHAMPION string = "v1.2/champion/"
-    STATIC_ITEM     string = "v1.2/item/"
+	STATS_RANKED  string = "v1.3/stats/by-summoner/"
+	STATS_SUMMARY string = "v1.3/stats/by-summoner/"
+
+	STATIC_VERSION  string = "v1.2/versions/"
+	STATIC_CHAMPION string = "v1.2/champion/"
+	STATIC_ITEM     string = "v1.2/item/"
 )
 
 func NormalizeNames(names ...string) []string {
-    nameList := []string(names)
+	nameList := []string(names)
 
-    for i, name := range nameList {
-        name = strings.Replace(name, " ", "", -1)
-        name = strings.ToLower(name)
-        nameList[i] = name
-    }
+	for i, name := range nameList {
+		name = strings.Replace(name, " ", "", -1)
+		name = strings.ToLower(name)
+		nameList[i] = name
+	}
 
-    return nameList
+	return nameList
 }
 
 func createApiUrl(endpoint string, region string) string {
-    return fmt.Sprintf("https://%v.api.pvp.net/api/lol/%v/%v", region, region, endpoint)
+	return fmt.Sprintf("https://%v.api.pvp.net/api/lol/%v/%v", region, region, endpoint)
 }
 
 func createStaticUrl(endpoint string, region string) string {
-    return fmt.Sprintf("https://%v.api.pvp.net/api/lol/static-data/%v/%v", region, region, endpoint)
+	return fmt.Sprintf("https://%v.api.pvp.net/api/lol/static-data/%v/%v", region, region, endpoint)
+}
+
+func createChampMasteryUrl(endpoint string, region string) string {
+	return fmt.Sprintf("https://%v.api.pvp.net/championmastery/location/%v", region, pidMap[region], endpoint)
 }
 
 func createObserverUrl(endpoint string, region string) string {
-    return fmt.Sprintf("https://%v.api.pvp.net/observer-mode/rest/%v", region, endpoint)
+	return fmt.Sprintf("https://%v.api.pvp.net/observer-mode/rest/%v", region, endpoint)
 }
 
 func createShardUrl(endpoint string, region string) string {
-    return fmt.Sprintf("https://%v.api.pvp.net/shards/%v", region, endpoint)
+	return fmt.Sprintf("https://%v.api.pvp.net/shards/%v", region, endpoint)
 }
 
 func createArgs(keys string, argVals ...interface{}) string {
-    keyArray := strings.Split(keys, ",")
-    args := "api_key=" + ApiKey
+	keyArray := strings.Split(keys, ",")
+	args := "api_key=" + ApiKey
 
-    for i, arg := range argVals {
-        var val string = ""
-        switch arg.(type) {
-        case bool:
-            val = strconv.FormatBool(arg.(bool))
-        case int:
-            if arg.(int) != -1 {
-                val = strconv.Itoa(arg.(int))
-            }
-        case int64:
-            if arg.(int64) != -1 {
-                val = strconv.FormatInt(arg.(int64), 10)
-            }
-        default:
-            val = arg.(string)
-        }
+	for i, arg := range argVals {
+		var val string = ""
+		switch arg.(type) {
+		case bool:
+			val = strconv.FormatBool(arg.(bool))
+		case int:
+			if arg.(int) != -1 {
+				val = strconv.Itoa(arg.(int))
+			}
+		case int64:
+			if arg.(int64) != -1 {
+				val = strconv.FormatInt(arg.(int64), 10)
+			}
+		default:
+			val = arg.(string)
+		}
 
-        args += fmt.Sprintf("&%v=%v", keyArray[i], val)
-    }
+		args += fmt.Sprintf("&%v=%v", keyArray[i], val)
+	}
 
-    return args
+	return args
 }
 
 func decodeRequest(region string, url string, v interface{}) error {
-    pid := pidMap[region]
-    requestChannelMap[pid] <- true
+	pid := pidMap[region]
+	requestChannelMap[pid] <- true
 
-    if Debug {
-        log.Println("decodeRequest: " + url)
-    }
+	if Debug {
+		log.Println("decodeRequest: " + url)
+	}
 
-    resp, err := http.Get(url)
-    if err != nil {
-        return err
-    }
-    defer resp.Body.Close()
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
 
-    if resp.StatusCode == http.StatusBadRequest {
-        log.Println("400 Bad request")
-        return errors.New("400")
-    } else if resp.StatusCode == http.StatusUnauthorized {
-        log.Println("401 Unauthorized - API key may not be set")
-        return errors.New("401")
-    } else if resp.StatusCode == http.StatusNotFound {
-        log.Println("404 No summoner data found for any specified inputs")
-        return errors.New("404")
-    } else if resp.StatusCode == 429 {
-        log.Println("429 Rate limit exceeded")
-        return errors.New("429")
-    } else if resp.StatusCode == http.StatusInternalServerError {
-        log.Println("500 Rito pls")
-        return errors.New("500")
-    } else if resp.StatusCode == http.StatusServiceUnavailable {
-        log.Println("503 Rito servers unavailable")
-        return errors.New("503")
-    }
+	if resp.StatusCode == http.StatusBadRequest {
+		log.Println("400 Bad request")
+		return errors.New("400")
+	} else if resp.StatusCode == http.StatusUnauthorized {
+		log.Println("401 Unauthorized - API key may not be set")
+		return errors.New("401")
+	} else if resp.StatusCode == http.StatusNotFound {
+		log.Println("404 No summoner data found for any specified inputs")
+		return errors.New("404")
+	} else if resp.StatusCode == 429 {
+		log.Println("429 Rate limit exceeded")
+		return errors.New("429")
+	} else if resp.StatusCode == http.StatusInternalServerError {
+		log.Println("500 Rito pls")
+		return errors.New("500")
+	} else if resp.StatusCode == http.StatusServiceUnavailable {
+		log.Println("503 Rito servers unavailable")
+		return errors.New("503")
+	}
 
-    err = json.NewDecoder(resp.Body).Decode(&v)
-    if err != nil {
-        return err
-    }
+	err = json.NewDecoder(resp.Body).Decode(&v)
+	if err != nil {
+		return err
+	}
 
-    return err
+	return err
 }
 
 func EncodeStruct(v interface{}) (string, error) {
-    jsonString, err := json.Marshal(v)
+	jsonString, err := json.Marshal(v)
 
-    return string(jsonString), err
+	return string(jsonString), err
 }
 
 func DecodeString(jsonString string, v interface{}) error {
-    err := json.Unmarshal([]byte(jsonString), v)
+	err := json.Unmarshal([]byte(jsonString), v)
 
-    return err
+	return err
 }
 
 func SetApiKey(key string) {
-    ApiKey = key
+	ApiKey = key
 }
 
 func SetDebug(debug bool) {
-    Debug = debug
+	Debug = debug
 }
 
 func SetRateLimit(rate int, delay int) {
-    for _, pid := range pidMap {
-        requestChannel := make(chan bool, rate)
-        requestChannelMap[pid] = requestChannel
-        go rateLimitHandler(requestChannel, delay)
-    }
+	for _, pid := range pidMap {
+		requestChannel := make(chan bool, rate)
+		requestChannelMap[pid] = requestChannel
+		go rateLimitHandler(requestChannel, delay)
+	}
 }
 
 func rateLimitHandler(requestChannel chan bool, delay int) {
-    for {
-        time.Sleep(time.Duration(delay) * time.Millisecond)
-        for i := 0; i < len(requestChannel); i++ {
-            <-requestChannel
-        }
-    }
+	for {
+		time.Sleep(time.Duration(delay) * time.Millisecond)
+		for i := 0; i < len(requestChannel); i++ {
+			<-requestChannel
+		}
+	}
 }
---
+++ summonermastery.go
@@ -0,0 +1,47 @@
+package legolegends
+
+import (
+	"fmt"
+)
+
+type ChampionMastery struct {
+	ChampionId                   int64  `json:"championId"`
+	ChampionLevel                int    `json:"championLevel`
+	ChampionPoints               int    `json:"championPoints`
+	ChampionPointsSinceLastLevel int64  `json:"championPointsSinceLastLevel`
+	ChampionPointsUntilNextLevel int64  `json:"championPointsUntilNextLevel`
+	ChestGranted                 bool   `json:"chestGranted`
+	HighestGrade                 string `json:"highestGrade`
+	LastPlayTime                 int64  `json:"lastPlayTime`
+	PlayerId                     int64  `json:"playerId`
+}
+
+func getChampionMastery(region string, summonerId int64, championId int64) (ChampionMastery, error) {
+	args := createArgs("")
+	url := createChampMasteryUrl(CHAMPION_MASTERY, region) + fmt.Sprintf("%d/champion/%d?%v", summonerId, championId, args)
+
+	var championMastery ChampionMastery
+	err := decodeRequest(region, url, &championMastery)
+
+	return championMastery, err
+}
+
+func getChampionMasteries(region string, summonerId int64) ([]ChampionMastery, error) {
+	args := createArgs("")
+	url := createChampMasteryUrl(CHAMPION_MASTERY, region) + fmt.Sprintf("%d/champions?%v", summonerId, args)
+
+	var championMastery []ChampionMastery
+	err := decodeRequest(region, url, &championMastery)
+
+	return championMastery, err
+}
+
+func getSpecificChampionMastery(region string, summonerId int64, count int) ([]ChampionMastery, error) {
+	args := createArgs("count", count)
+	url := createChampMasteryUrl(CHAMPION_MASTERY, region) + fmt.Sprintf("%d/topchampions?%v", summonerId, args)
+
+	var championMastery []ChampionMastery
+	err := decodeRequest(region, url, &championMastery)
+
+	return championMastery, err
+}
